5230a6be2c1ac9161b88b24ae9992cef18312884
huan <978252909@qq.com>
2021-10-20 12:48:24 +0800

    2021 10 20

--- qdRouter_library/src/main/java/cn/demomaster/qdrouter_library/actionbar/QuickAppBarLayout.java
+++ qdRouter_library/src/main/java/cn/demomaster/qdrouter_library/actionbar/QuickAppBarLayout.java
@@ -14,51 +14,56 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import cn.demomaster.qdrouter_library.R;
+import cn.demomaster.qdrouter_library.util.DisplayUtil;
+import cn.demomaster.qdrouter_library.util.StatusBarUtil;
 
 public class QuickAppBarLayout extends FrameLayout {
     public QuickAppBarLayout(@NonNull Context context) {
         super(context);
-        init(context,null);
+        init(context, null);
     }
 
     public QuickAppBarLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        init(context,attrs);
+        init(context, attrs);
     }
 
     public QuickAppBarLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context,attrs);
+        init(context, attrs);
     }
 
     public QuickAppBarLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-        init(context,attrs);
+        init(context, attrs);
     }
+
     View view;
+
     private void init(Context context, AttributeSet attrs) {
-        /*if (attrs != null) {
+        if (attrs != null) {
             TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.QDActionBarLayout);
             int layoutId = typedArray.getResourceId(R.styleable.QDActionBarLayout_android_layout, R.layout.qd_activity_actionbar_common);
-            view = LayoutInflater.from(context).inflate(layoutId, null, false);
             typedArray.recycle();
+            view = LayoutInflater.from(context).inflate(layoutId, null, false);
             addView(view);
-        }*/
-
+            setPadding(getPaddingLeft(), DisplayUtil.getStatusBarHeight(context), getPaddingRight(), getPaddingBottom());
+        }
+        
         ViewCompat.setOnApplyWindowInsetsListener(
                 this,
                 new androidx.core.view.OnApplyWindowInsetsListener() {
                     @Override
                     public WindowInsetsCompat onApplyWindowInsets(
                             View v, @NonNull WindowInsetsCompat insets) {
-                       // view.setPadding(view.getPaddingLeft(),lastInsets.getSystemWindowInsetTop(),view.getPaddingRight(),view.getPaddingBottom());
                         return onWindowInsetChanged(insets);
                     }
                 });
     }
 
+    @Nullable
+    WindowInsetsCompat lastInsets;
 
-    @Nullable WindowInsetsCompat lastInsets;
     WindowInsetsCompat onWindowInsetChanged(@NonNull final WindowInsetsCompat insets) {
         WindowInsetsCompat newInsets = null;
 
@@ -73,6 +78,7 @@ public class QuickAppBarLayout extends FrameLayout {
             requestLayout();
         }
 
+        setPadding(getPaddingLeft(), lastInsets.getSystemWindowInsetTop(), getPaddingRight(), getPaddingBottom());
         // Consume the insets. This is done so that child views with fitSystemWindows=true do not
         // get the default padding functionality from View
         return insets.consumeSystemWindowInsets();
